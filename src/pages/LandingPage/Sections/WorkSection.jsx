12f638f3140a0324f47346c3854fa20e8f78de35
Steinar Bragi Sigurðarson <steinar.bragi@gmail.com>
2019-08-26 13:21:23 +0000

    added hidden field for netlify

--- src/pages/LandingPage/Sections/WorkSection.jsx
+++ src/pages/LandingPage/Sections/WorkSection.jsx
@@ -23,7 +23,7 @@ class WorkSection extends React.Component {
             <h4 className={classes.description}>
               Would you like to collaborate with me? Drop me a line with a summary of your project, product or agency. I'll respond asap and we can arrange a meeting!
             </h4>
-            <form name="contact" method="POST" data-netlify="true">
+            <form netlify name="contact" method="POST" data-netlify="true">
               <GridContainer>
                 <GridItem xs={12} sm={12} md={6}>
                   <CustomInput
@@ -69,6 +69,7 @@ class WorkSection extends React.Component {
                   </GridItem>
                 </GridContainer>
               </GridContainer>
+              <input type="hidden" name="contact" value="contact" />
             </form>
           </GridItem>
         </GridContainer>
