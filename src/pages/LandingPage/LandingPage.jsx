994f639faf4de0e8caf87acb3614e730b102dccd
Steinar Bragi Sigurðarson <steinar.bragi@gmail.com>
2019-08-26 21:15:33 +0000

    subheading

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -50,7 +50,7 @@ class LandingPage extends React.Component {
               <GridItem xs={12} sm={12} md={6}>
                 <h1 className={classes.title}>Hi, I'm Steinar</h1>
                 <h4>
-                I create web and mobile experiences for the next generation of consumer-facing companies. I love to play with bleeding edge web technologies and I live to tinker and learn. To me, every successful day involves learning something new. My stack these days includes React, Flutter, Node.js, MongoDB and Serverless. I've also studied machine learning and advanced data analytics.
+                I create web and mobile experiences for the next generation of consumer-facing companies. I love to play with bleeding edge web technologies and I live to tinker and learn. My stack these days includes React, Flutter, Node.js, MongoDB and Serverless.
                 </h4>
                 <br />
                 {/*<Button
