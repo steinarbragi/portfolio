3cfec00a3ec093b93c0656392d0e25563ec5de41
Steinar Bragi Sigurðarson <steinar.bragi@gmail.com>
2019-08-26 13:56:25 +0000

    added icons and update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,22 @@
 module.exports = {
   pathPrefix: `/mini-gatsbyv2-material-kit-react`,
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    // Used for the title template on pages other than the index site
+    siteTitle: `steinar.io`,
+    // Default title of the page
+    siteTitleAlt: `steinar.io - Web & Mobile development`,
+    // Can be used for e.g. JSONLD
+    siteHeadline: `steinar.io - Web & Mobile development`,
+    // Will be used to generate absolute URLs for og:image etc.
+    siteUrl: `https://steinar.io`,
+    // Used for SEO
+    siteDescription: `steinar.io - I create web and mobile experiences for the next generation of consumer-facing companies.`,
+    // Will be set on the <html /> tag
+    siteLanguage: `en`,
+    // Used for og:image and must be placed inside the `static` folder
+    siteImage: `/banner.jpg`,
+    // Twitter Handle
+    author: `@steinarbragi`
   },
   plugins: [
     'gatsby-plugin-resolve-src',
@@ -12,14 +27,25 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Steinar Bragi Sigurðarson',
+        description: `Web and Mobile developer`,
+        short_name: 'steinar',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/assets/img/favicon.png', // This path is relative to the root of the site.
-      },
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],      },
     },
   ],
 }
