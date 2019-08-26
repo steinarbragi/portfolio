7249c16fbbbdd65a517402f11714c52911985c43
Steinar Bragi Sigurðarson <steinar.bragi@gmail.com>
2019-08-26 18:53:15 +0000

    cloud description

--- src/pages/LandingPage/Sections/ProductSection.jsx
+++ src/pages/LandingPage/Sections/ProductSection.jsx
@@ -49,7 +49,7 @@ class ProductSection extends React.Component {
             <GridItem xs={12} sm={12} md={4}>
               <InfoArea
                 title="Cloud"
-                description="In 2010 I managed cPanel hosting for a small internet company. Since then I've used everything from Heroku, AWS, Serverless, MongoDB Atlas, etc. I know how to set up and maintain CI/CD and databases."
+                description="In 2010 I managed cPanel hosting for a small internet company. Since then I've used everything from Heroku, AWS, Serverless, MongoDB Atlas, etc. I know how to maintain CI/CD pipelines and build APIs."
                 icon={Cloud}
                 iconColor="danger"
                 vertical
