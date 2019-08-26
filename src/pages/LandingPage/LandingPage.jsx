26d2656a363ffee85aa3a534682cdf6bdc70ca52
Steinar Bragi Sigurðarson <steinar.bragi@gmail.com>
2019-08-26 15:41:46 +0000

    added title to helmet

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -45,6 +45,7 @@ class LandingPage extends React.Component {
           {...rest}
         />
         <Helmet>
+          <title>Steinar.io</title>
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
